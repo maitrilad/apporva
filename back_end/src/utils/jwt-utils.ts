73a3696823ee690c162979d3d6ef0a1217bd2361
Rishika Bothra <rshkbothra@gmail.com>
2026-02-09 21:27:07 +0530

    Resolved the error and formated the file

--- back_end/src/utils/jwt-utils.ts
+++ back_end/src/utils/jwt-utils.ts
@@ -22,10 +22,17 @@ export function generateToken(payload: { id: number, role: Role }): string {
   return token;
 }
 
+<<<<<<< HEAD
 export function verifyToken(token: string): { id: number; role: Role } {
   try {
     const decoded = jwt.verify(token, env.JWT_SECRET) as {
       id: number;
+=======
+export function verifyToken(token: string): { user_id: string; role: Role } {
+  try {
+    const decoded = jwt.verify(token, env.JWT_SECRET) as {
+      user_id: string;
+>>>>>>> 7c1e84b (Resolved the error and formated the file)
       role: Role;
     };
 
