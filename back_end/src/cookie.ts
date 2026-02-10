00ff75be3eed39bb725720c5de11d78cd58332bc
Rishika Bothra <rshkbothra@gmail.com>
2026-02-10 19:31:07 +0530

    fix:update cookie security logic

--- back_end/src/cookie.ts
+++ back_end/src/cookie.ts
@@ -1,14 +1,14 @@
 import { Response,Request } from "express";
 import { env } from "../src/config/env";
+import dotenv from "dotenv";
+dotenv.config();
 
 const COOKIE_NAME = "token";
-
-const isprod = env.NODE_ENV === "prod";
-
 
 function isHttps(req: Request): boolean {
     return req.secure || req.headers["x-forwarded-proto"] === "https";
     }
+const isprod = process.env.NODE_ENV === "prod";
 
 export function setAuthCookie(
     req: Request,
@@ -17,11 +17,7 @@ export function setAuthCookie(
 ){
     res.cookie(COOKIE_NAME, token, {
         httpOnly: true,
-<<<<<<< HEAD
-        secure: isprod,
-=======
         secure: isHttps(req),
->>>>>>> 1de8b50 (feat: add cookie-based auth middleware with secure detection)
         sameSite: "lax",
     });
 }
